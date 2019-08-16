fdc63ec34a25266b81573e9bf0f34b34c6d98901
DraganJankovic997 <gagibratee@gmail.com>
2019-08-16 11:51:53 +0200

    Confirm password field in register added

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,6 +4,7 @@
         <p>Name: <input v-model="user.name" type="text"></p>
         <p>Email: <input v-model="user.email" type="text"></p>
         <p>Password: <input v-model="user.password" type="password"></p>
+        <p>Confirm password: <input v-model="user.password_confirmation" type="password"></p>
         <button @click="callRegister">Register</button>
     </div>
 </template>
@@ -18,7 +19,8 @@ export default {
             user : {
                 name: 'novitest',
                 email: 'novitest@gmail.com',
-                password: 'password'
+                password: 'password',
+                password_confirmation: 'password'
             }
         }
     },
